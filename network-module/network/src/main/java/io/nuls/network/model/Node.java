797bec4585cbd462de737d48547e65c2f140d81f
vivi <251556768@qq.com>
2018-05-23 10:12:52 +0800

    account ledger

--- network-module/network/src/main/java/io/nuls/network/model/Node.java
+++ network-module/network/src/main/java/io/nuls/network/model/Node.java
@@ -139,7 +139,7 @@ public class Node extends BaseNulsData {
 
     public void destroy() {
         this.lastFailTime = TimeService.currentTimeMillis();
-        failCount += 1;
+        this.setFailCount(this.getFailCount() + 1);
         this.status = Node.CLOSE;
     }
 
